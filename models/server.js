0249bcef634b15d0bc59c33ccb6205df505a8352
Luis Zelarrayan <luiszelarrayan@hotmail.com>
2021-07-06 15:03:46 -0300

    primer commit

---
+++ models/server.js
@@ -0,0 +1,55 @@
+//Servidor de express
+
+const express  = require('express');
+const http     = require('http')
+const socketio = require('socket.io')
+const path     = require('path');
+const sockets  = require('./sockets');
+
+
+class Server{
+
+    // propiedades
+    constructor(){
+        
+        this.app =  express();
+        this.port = process.env.PORT; // esto lee el puerto del archivo .env
+
+        //Http server
+        this.server = http.createServer(this.app)
+
+        //Configuración del socket 
+        this.io = socketio(this.server, {/* configuraciones */});
+
+    }
+
+    // metodos
+
+    middlewares(){
+        //Desplegar el directorio publico
+        this.app.use(express.static( path.resolve(__dirname, '../public') ) );
+    }
+
+    configurarSockets(){
+        new sockets( this.io);
+    }
+
+    execute(){
+        // Inicializar middlewares
+        this.middlewares();
+
+        //Inicializar sockets
+        this.configurarSockets();
+
+        //Inicializar Server
+        this.server.listen(this.port,()=>{
+        console.log('server corriendo en puerto:', this.port)
+    });
+    
+   }
+
+    
+
+}
+
+module.exports = Server; // exporto la clase Server
